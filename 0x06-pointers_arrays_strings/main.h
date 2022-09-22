e0ec6c73f468f381576d284a9f9c2992ee24a46f
MaleleD <desmondmalele@gmail.com>
2022-09-22 15:46:46 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * file: main.h
+ * By: Julien Barbier
+ * Desc: Header file containing prototypes for all functions
+ * 	 used in the 0x06-pointers_arrays_strings
+ */
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
