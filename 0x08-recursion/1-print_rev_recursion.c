0b933b92f85376fce52118807bb58c2963edfff1
MaleleD <desmondmalele@gmail.com>
2022-09-29 13:49:15 -0700

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,14 +3,14 @@
 /**
  * _print_rev_recursion - a function that prints a string in reverse
  * @s: string
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately
+ *
+ * Return: void
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		if (*s != '\0')
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
