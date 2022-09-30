aa58c0e73500318da2ea65cabedf505469d31bd0
MaleleD <desmondmalele@gmail.com>
2022-09-29 20:54:27 -0700

    task5

--- 0x08-recursion/5-sqrt_recusrsion.c
+++ 0x08-recursion/5-sqrt_recusrsion.c
@@ -1,27 +1,27 @@
-#include "main.h"                                                                                                                       
-#include <stdio.h>                                                                                                                      
+#include "main.h" 
+#include <stdio.h>
 
-/**                                                                                                                                     
- * _sqrt_recursion - a function that returns the natural square root of a number                                                        
- * @n: number to be used                                                                                                                
+/**
+ * _sqrt_recursion - a function that returns the natural square root of a number
+ * @n: number to be used
  *
- * Return: the square root of n                                                                                                         
- */                                                                                                                                     
-int _sqrt_recursion(int n)                                                                                                              
-{                                                                                                                                       
-
-	if (n == 1 || n == 0)                                                                                                           		return (n);
+ * Return: the square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 1 || n == 0)
+		return (n);
 	return (_sqrt(0, n));
-}                                                                                                           
-/**                                                                                                                                     
- * _sqrt - returns the square root of a number                                                                                          
- * @n: test number                                                                                                                      
- * @x: squared number                                                                                                                   
- 
- * Return: the square root of n                                                                                                         
- */                                                                                                                                     
-int _sqrt(int n, int x)                                                                                                                 
-{                                                                                                                                       
+}
+/**
+ * _sqrt - returns the square root of a number
+ * @n: test number
+ * @x: squared number
+ *
+ * Return: the square root of n
+ */
+int _sqrt(int n, int x)
+{
 	if (n > x / 2)
 		return (-1);
 	else if (n * n == x)
