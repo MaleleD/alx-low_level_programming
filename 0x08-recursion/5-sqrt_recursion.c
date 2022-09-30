f278fb8727b39aa99014e238a7ec66725a587539
MaleleD <desmondmalele@gmail.com>
2022-09-29 20:23:59 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,28 +2,28 @@
 
 /**
  * sqrt2 - a function that returns the natural square root of a number
- * @a: same number as n
- * @b: number that iterates from 1 to n
+ * @n: number to be used
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: the square root of n
  */
 int _sqrt_recursion(int n)
 {
-	if (b * b == a)
-		return (b);
-	else if (b * b > a)
-		return (-1);
-	return (sqrt2(a, b + 1));
+	if (n == 1 || n == 0)
+		return (n);
+	return (sqrt(0, n));
 }
 /**
- * _sqrt_recursion - returns the natural square root of n
- * @n: Number Integer
+ * _sqrt_recursion - returns the square root of a number
+ * @n: test number
+ * @x: squared number
  *
- * Return: On success 1
- * On error, -1 is returned, and error is set appropriately
+ * Return: the square root of n
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrt2(n, 1));
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
