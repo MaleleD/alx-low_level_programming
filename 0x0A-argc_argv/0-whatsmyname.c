1d221dff13e93797266cdf71b061124f1c5c1537
MaleleD <desmondmalele@gmail.com>
2022-09-30 15:46:21 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ * @argc: the argument count
+ * @argv: the argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[]);
+	(void)argc;
+	return (0);
+}
