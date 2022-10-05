e89908812130c2c23b82e7d29cf6e7ad35cde48a
MaleleD <desmondmalele@gmail.com>
2022-10-05 02:33:02 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,14 +4,14 @@
 /**
  * str_concat - a function that concatenates two strings
  * @s1: input one to concat
- * @s@: input two to concat
+ * @s2: input two to concat
  * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 	int i, ci;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
