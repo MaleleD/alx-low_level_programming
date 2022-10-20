25c8635e49d10b47a53a73ddcb1dce18c8162049
MaleleD <desmondmalele@gmail.com>
2022-10-20 12:35:30 -0700

    task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning
+ * add_node - Adds a new node at the beginning of a list_t list.
  * @head: A pointer to the head of the list_t list.
  * @str: The string to be added to the list_t list.
  *
@@ -25,7 +25,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	for (len = 0; str[len];
+	for (len = 0; str[len];)
 		len++;
 
 	new->str = dup;
