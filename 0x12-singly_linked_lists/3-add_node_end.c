37851f1a6e2cc46e41879ce0290c2656b3ec9483
MaleleD <desmondmalele@gmail.com>
2022-10-20 12:28:06 -0700

    task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -6,7 +6,7 @@
  * @head: A pointer the head of the list_t list.
  * @str: The string to be added to the list_t list.
  *
- * Return: If the function fails - NULL. Otherwise - the address of the new element.
+ * Return: NULL if fail, Otherwise address of new element.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
